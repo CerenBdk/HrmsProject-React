f426f9d54a8c0fd937dc9ef89cdb72816b0498e5
CerenBdk <cerenbdk@gmail.com>
2021-06-06 22:21:50 +0300

    Section has been updated.

--- src/layouts/Section.jsx
+++ src/layouts/Section.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import JobAdvertList from "../pages/JobAdvert/JobAdvertList";
+import JobPositionList from "../pages/JobAdvert/JobPosition/JobPositionList";
+import EmployerList from "../pages/User/Employer/EmployerList";
+import JobseekerList from "../pages/User/Jobseeker/JobseekerList";
 
 export default function Section() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Grid>
+        <Grid.Row>
+          <GridColumn size={14}>
+            <JobseekerList />
+          </GridColumn>
+        </Grid.Row>
+        <Grid.Row>
+          <GridColumn size={14}>
+            <EmployerList />
+          </GridColumn>
+        </Grid.Row>
+        <Grid.Row>
+          <GridColumn size={14}>
+            <JobAdvertList />
+          </GridColumn>
+        </Grid.Row>
+        <Grid.Row>
+          <GridColumn size={14}>
+            <JobPositionList />
+          </GridColumn>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 }
