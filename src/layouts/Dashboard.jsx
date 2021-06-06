6aaa1aef0bf459d8b51307bfd26124f199c6a550
CerenBdk <cerenbdk@gmail.com>
2021-06-06 21:48:06 +0300

    Dashboard has been updated.

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
-import JobAdvertList from "../pages/JobAdvert/JobAdvertList";
-import JobPositionList from "../pages/JobAdvert/JobPosition/JobPositionList";
-import EmployeeList from "../pages/User/Employee/EmployeeList";
-import EmployerList from "../pages/User/Employer/EmployerList";
-import JobseekerList from "../pages/User/Jobseeker/JobseekerList";
+import Section from "./Section";
 import SideBar from "./SideBar";
 
 export default function Dashboard() {
@@ -16,7 +12,7 @@ export default function Dashboard() {
             <SideBar/>
           </Grid.Column>
           <Grid.Column width={14}>
-              <JobAdvertList/>
+              <Section/>
           </Grid.Column>
         </Grid.Row>
       </Grid>
